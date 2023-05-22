f9ddc7275ef97d536c906233b69d608ff3250e6f
Daniel Kogan <danielkoganx@gmail.com>
2023-05-21 21:20:12 -0400

    Update cli.h

--- include/cli.h
+++ include/cli.h
@@ -1,8 +1,3 @@
-/*
- * DO NOT MODIFY THE CONTENTS OF THIS FILE.
- * IT WILL BE REPLACED DURING GRADING
- */
-
 WATCHER *cli_watcher_start(WATCHER_TYPE *type, char *args[]);
 int cli_watcher_stop(WATCHER *wp);
 int cli_watcher_send(WATCHER *wp, void *msg);
