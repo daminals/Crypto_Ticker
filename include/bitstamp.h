f71262ac8c695cfac1eb2ea27ac29bf99d76a1f2
Daniel Kogan <danielkoganx@gmail.com>
2023-05-21 21:21:08 -0400

    Update bitstamp.h

--- include/bitstamp.h
+++ include/bitstamp.h
@@ -1,8 +1,3 @@
-/*
- * DO NOT MODIFY THE CONTENTS OF THIS FILE.
- * IT WILL BE REPLACED DURING GRADING
- */
-
 WATCHER *bitstamp_watcher_start(WATCHER_TYPE *type, char *args[]);
 int bitstamp_watcher_stop(WATCHER *wp);
 int bitstamp_watcher_send(WATCHER *wp, void *msg);
